55dfd5e9ad8d2ebc341f9c3ec2c869a22fb1a75b
vMarti35 <victor.o.martinez.2002@gmail.com>
2025-02-24 13:29:24 -0500

    Update Doctors.java
    Added selectDB and updateDB to Doctors BO

--- BO_Chiro/src/main/java/org/example/Doctors.java
+++ BO_Chiro/src/main/java/org/example/Doctors.java
@@ -53,5 +53,72 @@ public class Doctors {
     public void setEmail(String email) {Email = email;}                     //Email
     public void setAdmin(Boolean admin) {Admin = admin;}                    //Admin
 
+    public void selectDB(Int docid) {
+        DocID = docid;
+
+        try {
+            //Load Driver
+
+            //Creating Connection
+
+            //SqL statement
+            String sql = "SELECT * FROM Doctors WHERE DocID = '" + docid + "'";
+            System.out.println(sql);
+
+            //Create Statement
+            Statement stmt = con.createStatement();
+
+            //Execute Statement
+            ResultSet rs;
+            rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                docid = rs.getInt(1);
+                pwd = rs.getInt(2);
+                firstname = rs.getString(3);
+                lastname = rs.getString(4);
+                email = rs,getString(5);
+                admin = rs.getBoolean(6);
+            }
+
+            //Close Connection
+            con.close();
+        } catch (Exception e) {
+            System.out.println("Exception:" + e);
+        }//end try/catch
+                
+    }//end selectDB()
+
+    public void updateDB(Int docid) {
+        DocID = docid;
+
+        try {
+            //Load Driver
+
+            //Creating Connection
+
+            //Create Statement
+            PreparedStatement stmt = con.prepareStatement(sql);
+            
+            //SQL statement
+            String sql = ("UPDATE Doctors SET Pwd = ?, FirstName = ?, LastName = ?, Email = ? WHERE DocID = ?")
+
+            stmt.setInt(2, );
+            stmt.setString(3, );
+            stmt.setString(4, );
+            stmt.setString(5, );
+
+
+            //Execute Statement
+            stmt.executeUpdate();
+
+            //Close Connection
+            con.close();
+            
+        } catch (Exception e) {
+            System.out.println("Exception:" + e);
+        }//end try/catch
+        
+    }//end updateDB()
 
 }
