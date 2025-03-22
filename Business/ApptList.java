9b0912ed9a269b9bf5abd076d1636afb9243a66f
callanb5 <140440498+callanb5@users.noreply.github.com>
2025-03-22 17:56:24 -0400

    Switch to ArrayList, add selectDBPatId() and selectDBDocId(), documentation, and bugfixes for ApptList.java
    - Fixes bug with ApptList being unable to correctly add appointments to the database.
    - Fixes error in display method.
    - Switches to ArrayList for better storing of appointments (removes cap of 10)
    - Adds selectDBPatId() and selectDBDocId() which recursively select from the database and add to the ArrayList.
    - Adds some documentation

--- Business/ApptList.java
+++ Business/ApptList.java
@@ -1,23 +1,114 @@
-package org.example.chiropractorproject.BusinessObjects;
+package org.example;
+
+/**
+ * ASP (Chiropractor)
+ * Last Editor: Callan Bramblett
+ * Date: 3/22/2025
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
 
 public class ApptList {
+    /**
+     * Number of appointments
+     */
     public int count;
-    public Appointments apptArr[] = new Appointments[10];
+
+    /**
+     * Appointment array list.
+     */
+    public ArrayList<Appointments> appArrayList;
 
     public ApptList() {
-
+        count = 0;
+        appArrayList = new ArrayList<Appointments>();
     }
 
     public void addAppointment(Appointments a1) {
-        apptArr[count] = a1;
+        appArrayList.add(a1);
         count++;
     }
 
     public void displayList() {
         System.out.println("# of Appointments: " + count);
-
-        for (int x = 0; x < count; x++) {
-            apptArr[x].display();
+        
+        for (int x = 0; x < appArrayList.size(); x++) {
+            this.appArrayList.get(x).display();
+            
         }
+    }
+    
+   /**
+     * Recursively selects appointments from the database by patient id and adds to appArrayList.
+     * @param patid patient id
+     */
+    public void selectDBPatId(String patid) {
+        try {
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+
+            Connection con = DriverManager.getConnection("jdbc:ucanaccess://C:/ChiropracticDB.accdb");
+
+            Statement statement = con.createStatement();
+
+            String sql = "SELECT * FROM Appointments WHERE PatId='" + patid + "'";
+
+            ResultSet rs = statement.executeQuery(sql);
+            while(rs.next()) {
+                Appointments a = new Appointments();
+                a.setApptID(rs.getInt(1));
+                a.setApptDateTime(rs.getTimestamp(2));
+                a.setPatID(rs.getString(3));
+                a.setDocID(rs.getString(4));
+                a.setNotes(rs.getString(5));
+                addAppointment(a);
+            }
+            
+            con.close();
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
+    
+       /**
+     * Recursively selects appointments from the database by doctor id and adds to appArrayList.
+     * @param docid doctor id
+     */
+    public void selectDBDocId(String docid) {
+        try {
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+
+            Connection con = DriverManager.getConnection("jdbc:ucanaccess://C:/ChiropracticDB.accdb");
+
+            Statement statement = con.createStatement();
+
+            String sql = "SELECT * FROM Appointments WHERE DocID='" + docid + "'";
+
+            ResultSet rs = statement.executeQuery(sql);
+            while(rs.next()) {
+                Appointments a = new Appointments();
+                a.setApptID(rs.getInt(1));
+                a.setApptDateTime(rs.getTimestamp(2));
+                a.setPatID(rs.getString(3));
+                a.setDocID(rs.getString(4));
+                a.setNotes(rs.getString(5));
+                addAppointment(a);
+            }
+            
+            con.close();
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
+    
+    public static void main(String[] args) {
+        ApptList al = new ApptList();
+        al.selectDBDocId("001");
+        al.displayList();       
     }
 }
