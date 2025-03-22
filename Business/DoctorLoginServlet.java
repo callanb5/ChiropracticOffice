cf98205925d359e04ee6aefb2b59c0b892d22ecd
vMarti35 <victor.o.martinez.2002@gmail.com>
2025-03-22 18:00:37 -0400

    Add files via upload

---
+++ Business/DoctorLoginServlet.java
@@ -0,0 +1,75 @@
+package org.example.chiropractorproject;
+/*
+    Last Modified: 3/17/2025
+    Author: Victorino Martinez
+*/
+import java.io.*;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import org.example.chiropractorproject.BusinessObjects.Doctors;
+
+@WebServlet(name = "DoctorLoginServlet", urlPatterns = {"/DoctorLoginServlet"})
+public class DoctorLoginServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String id = "";
+        String pw = "";
+
+        try {
+            id = request.getParameter("CID");
+            pw = request.getParameter("PASS");
+            System.out.println("---Password Received---");
+
+            //Doctor Business Object
+            Doctors d1;
+            d1 = new Doctors();
+
+            //getting Doctor info from Doctors
+            d1.selectDB(id);
+            String CID = d1.getdocid();
+            String PASS = d1.getpwd();
+            System.out.println("Customer ID: " + d1.getdocid());
+            System.out.println("Password: " + d1.getpwd());
+
+            //Putting Customer object into the Session
+            HttpSession ses1;
+            ses1 = request.getSession();
+            ses1.setAttribute("d1", d1);
+            System.out.println("Patient added to Session/scheduling DoctorHomePage.jsp");
+
+
+            //forward page to AccountLookup.jsp if login matches database/ else forward to ErrorPage.jsp
+            if (pw.equals(PASS) && id.equals(CID)) {
+                RequestDispatcher rd = request.getRequestDispatcher("/DoctorHomePage.jsp");
+                rd.forward(request, response);
+
+            } else {
+                RequestDispatcher rd = request.getRequestDispatcher("/ErrorPage.jsp");
+                rd.forward(request, response);
+            }
+
+
+        } finally {
+            out.close();
+
+        }//end try/finally
+
+    }//end processRequest()
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+
+    }
+
+}//end LoginServlet()
---
+++ Business/PatientLoginServlet.java
@@ -0,0 +1,75 @@
+package org.example.chiropractorproject;
+/*
+    Last Modified: 3/17/2025
+    Author: Victorino Martinez
+*/
+import java.io.*;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import org.example.chiropractorproject.BusinessObjects.Patients;
+
+@WebServlet(name = "PatientLoginServlet", urlPatterns = {"/PatientLoginServlet"})
+public class PatientLoginServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String id = "";
+        String pw = "";
+
+        try {
+            id = request.getParameter("CID");
+            pw = request.getParameter("PASS");
+            System.out.println("---Password Received---");
+
+            //Patients Business Object
+            Patients p1;
+            p1 = new Patients();
+
+            //getting Customer info from CustomerBO
+            p1.selectDB(id);
+            String CID = p1.getpatid();
+            String PASS = p1.getpwd();
+            System.out.println("Customer ID: " + p1.getpatid());
+            System.out.println("Password: " + p1.getpwd());
+
+            //Putting Customer object into the Session
+            HttpSession ses1;
+            ses1 = request.getSession();
+            ses1.setAttribute("p1", p1);
+            System.out.println("Customer added to Session/scheduling PatientHomePage.jsp");
+
+
+            //forward page to AccountLookup.jsp if login matches database/ else forward to ErrorPage.jsp
+            if (pw.equals(PASS) && id.equals(CID)) {
+                RequestDispatcher rd = request.getRequestDispatcher("/PatientHomePage.jsp");
+                rd.forward(request, response);
+
+            } else {
+                RequestDispatcher rd = request.getRequestDispatcher("/ErrorPage.jsp");
+                rd.forward(request, response);
+            }
+
+
+        } finally {
+            out.close();
+
+        }//end try/finally
+
+    }//end processRequest()
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+
+    }
+
+}//end LoginServlet()
