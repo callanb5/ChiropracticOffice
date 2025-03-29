1df760ddb43bc0af4df00df9ed76fc425c0fc959
callanb5 <140440498+callanb5@users.noreply.github.com>
2025-03-29 16:06:10 -0400

    Change PatientList.java to ArrayList
    Changes PatientList.java to ArrayList and makes needed changes for this.

--- Business/PatientList.java
+++ Business/PatientList.java
@@ -1,23 +1,38 @@
-package org.example.chiropractorproject.BusinessObjects;
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class PatientList {
+    /**
+     * Number of patients
+     */
     public int count;
-    public Patients patArr[] = new Patients[10];
+
+    /**
+     * Patient array list.
+     */
+    public ArrayList<Patients> patArrayList;
 
     public PatientList() {
-
+        count = 0;
+        patArrayList = new ArrayList<Patients>();
     }
 
-    public void addPatient(Patients p1) {
-        patArr[count] = p1;
+    public void addAppointment(Patients a1) {
+        patArrayList.add(a1);
         count++;
     }
-
+    
     public void displayList() {
         System.out.println("# of Patients: " + count);
-
-        for (int x = 0; x < count; x++) {
-            patArr[x].display();
+        
+        for (int x = 0; x < patArrayList.size(); x++) {
+            this.patArrayList.get(x).display();
+            
         }
-    }
+    }   
 }
