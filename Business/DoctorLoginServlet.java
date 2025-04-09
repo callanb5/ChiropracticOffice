c81333efad36a1820dce3fa8cbbb2e56257f3381
vMarti35 <victor.o.martinez.2002@gmail.com>
2025-04-09 15:21:50 -0400

    Update DoctorLoginServlet.java

--- Business/DoctorLoginServlet.java
+++ Business/DoctorLoginServlet.java
@@ -34,12 +34,12 @@ public class DoctorLoginServlet extends HttpServlet {
             d1.selectDB(id);
             String CID = d1.getdocid();
             String PASS = d1.getpwd();
-            Boolean ADMIN = d1.getadmin();
+            String ADMIN = String.valueOf(d1.getadmin());
             System.out.println("Doctor ID: " + d1.getdocid());
             System.out.println("Password: " + d1.getpwd());
-            if (ADMIN = true) {
+            if (ADMIN == "true") {
                 System.out.println("Admin: True");
-            } else if (ADMIN = false) {
+            } else if (ADMIN == "false") {
                 System.out.println("Admin: False");
             }
 
@@ -51,16 +51,16 @@ public class DoctorLoginServlet extends HttpServlet {
 
 
             //forward page to Doctor-Home.jsp if login matches database/ else forward to ErrorPage.jsp
-            if (pw.equals(PASS) && id.equals(CID) && ADMIN.equals(false)) {
+            if (pw.equals(PASS) && id.equals(CID) && ADMIN.equals("false")) {
                 RequestDispatcher rd = request.getRequestDispatcher("/Doctor-Home.jsp");
                 rd.forward(request, response);
 
-            } else if (pw.equals(PASS) && id.equals(CID) && ADMIN.equals(true)) {
+            } else if (pw.equals(PASS) && id.equals(CID) && ADMIN.equals("true")) {
                 RequestDispatcher rd = request.getRequestDispatcher("/Admin-Home.jsp");
                 rd.forward(request, response);
 
             } else {
-                System.out.println("Error Occurred");
+                RequestDispatcher rd = request.getRequestDispatcher("/Error.jsp");
             }
 
 
